3f31ca05fade2081edcec0a4d6268f9abd34a48d
EnricoCirilli <enrico.cirilli@gmail.com>
2023-10-26 19:00:33 +0200

    add: readjusting scaffolding

---
+++ paridispari/scriptj.js
@@ -0,0 +1,7 @@
+const userNumber = parseInt(prompt("Dimmi un numero e ti dico se pari o dispari"));
+const result = oddEven(userNumber)
+console.log(result);
+
+function oddEven(numberToTest) {
+    let result = "";
+}
--- scriptj.js
+++
@@ -1,2 +0,0 @@
-const userNumber = parseInt(prompt("Dimmi un numero e ti dico se pari o dispari"));
-const result = oddEven(userNumber)
