3ff01ee1fc194a4da21034d66b1f582a5ac161a7
EnricoCirilli <enrico.cirilli@gmail.com>
2023-10-27 01:01:11 +0200

    add: oddeven

--- paridispari/scriptj.js
+++ paridispari/scriptj.js
@@ -1,7 +1,28 @@
-const userNumber = parseInt(prompt("Dimmi un numero e ti dico se pari o dispari"));
-const result = oddEven(userNumber)
-console.log(result);
+const userPariDispari = prompt( "scegli pari o dispari");
+const userNumber = prompt("scegli un numero da 1 a 5");
 
-function oddEven(numberToTest) {
-    let result = "";
+
+
+function getRandomNum(){
+   
+    let cpu =  Math.floor((Math.random() * 5) + 1);
+    let resultNumber = cpu + userNumber;
+  
+    if (resultNumber % 2 === 0){
+        result ="even";
+    }else {
+        result ="odd";
+    }
+   return result;
 }
+let message = 0;
+if(userNumber === "even" && resultNumber === "even"){
+    console.log("hai vinto");
+}else if (userNumber === "odd" && resultNumber === "odd"){
+    console.log("Hai vinto");
+}else{
+    console.log = ("hai perso!");
+}
+
+
+    
