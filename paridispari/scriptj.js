c658a034b46d13f7d7cca5ae9d8e9040a1d0a970
EnricoCirilli <enrico.cirilli@gmail.com>
2023-10-27 08:40:10 +0200

    add: revising trying to find a way out

--- paridispari/scriptj.js
+++ paridispari/scriptj.js
@@ -1,21 +1,24 @@
 const userPariDispari = prompt("scegli pari o dispari");
 const userNumber = prompt("scegli un numero da 1 a 5");
 console.log(userNumber);
+alert(userNumber);
 
-const resultNumber = (cpu + userNumber);
-console.log(resultNumber);
 
-function resultNumber() {
+/*const resultNumber = (cpu + userNumber);
+console.log(resultNumber);*/
+
+function resultNumber(numberToCheck) {
 
     let cpu = Math.floor((Math.random() * 5) + 1);
     let resultNumber = cpu + userNumber;
-    console.log(resultNumber);
+
     if (resultNumber % 2 === 0) {
         result = "even";
     } else {
         result = "odd";
     }
 
+    console.log(resultNumber);
 
     let message = 0;
     if (userNumber === "even" && resultNumber === "even") {
@@ -25,5 +28,5 @@ function resultNumber() {
     } else {
         console.log = ("hai perso!");
     }
-
+     return result;
 }
